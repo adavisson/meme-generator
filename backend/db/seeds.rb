6d6154699bb5cb09a3413cf5526fc63ad973900a
Andrew Davisson <akdavisson4@gmail.com>
2019-10-15 13:34:25 -0600

    Added seed data for images

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Picture.create(title: "Distracted Boyfriend", link: "https://imgflip.com/s/meme/Distracted-Boyfriend.jpg")
+Picture.create(title: "Mocking Spongebob", link: "https://imgflip.com/s/meme/Mocking-Spongebob.jpg")
+Picture.create(title: "Roll Safe Think About It", link: "https://imgflip.com/s/meme/Roll-Safe-Think-About-It.jpg")
+Picture.create(title: "Surprised Pikachu", link: "https://imgflip.com/s/meme/Surprised-Pikachu.jpg")
+Picture.create(title: "Is this a Pigeon", link: "https://imgflip.com/s/meme/Is-This-A-Pigeon.jpg")
+Picture.create(title: "Ancient Aliens", link: "https://imgflip.com/s/meme/Ancient-Aliens.jpg")
+Picture.create(title: "X, X Everywhere", link: "https://imgflip.com/s/meme/X-X-Everywhere.jpg")
+Picture.create(title: "One Does Not Simply", link: "https://imgflip.com/s/meme/One-Does-Not-Simply.jpg")
+Picture.create(title: "Third World Skeptical Kid", link: "https://imgflip.com/s/meme/Third-World-Skeptical-Kid.jpg")
+Picture.create(title: "Success Kid", link: "https://imgflip.com/s/meme/Success-Kid.jpg")
+Picture.create(title: "Creepy Condescending Wonka", link: "https://imgflip.com/s/meme/Creepy-Condescending-Wonka.jpg")
+Picture.create(title: "Shut Up and Take My Money Fry", link: "https://imgflip.com/s/meme/Shut-Up-And-Take-My-Money-Fry.jpg")
