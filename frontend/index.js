0a442dc6244247ad9f33f15d2c57cd6de421a710
Andrew Davisson <akdavisson4@gmail.com>
2019-10-15 14:11:16 -0600

    testing that fetch works for pictures

--- frontend/index.js
+++ frontend/index.js
@@ -1,11 +1,27 @@
+const BASE_URL = "http://localhost:3000";
+const PICTURES_URL = `${BASE_URL}/pictures`
+
+let image;
+
 document.addEventListener("DOMContentLoaded", function() {
   //loadMeme();
 });
 
+function getData() {
+  return fetch(`${PICTURES_URL}/2`)
+  .then(resp => resp.json())
+}
+
 function loadMeme() {
   let memeSpace = document.getElementById("content");
-  let header = document.createElement("h2");
-  header.innerHTML = "super rad meme";
+  let imgDiv = document.createElement("div");
+  imgDiv.setAttribute("class", "img");
+  let meme = document.createElement("img");
+  fetch(`${PICTURES_URL}/2`)
+    .then(resp => resp.json())
+    .then(json => {
+      meme.setAttribute("src", `${json.link}`);
+    });
   let deleteButton = document.createElement("button");
   deleteButton.innerHTML = "Get outta here!";
   deleteButton.setAttribute("class", "btn btn-primary");
@@ -14,7 +30,8 @@ function loadMeme() {
   });
   memeSpace.style.textAlign = "center";
   memeSpace.setAttribute("class", "meme-space");
-  memeSpace.appendChild(header);
+  imgDiv.appendChild(meme);
+  memeSpace.appendChild(imgDiv);
   memeSpace.appendChild(deleteButton);
 }
 
