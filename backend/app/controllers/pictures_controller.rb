b169685a3ebbcff2617766a57089e57d04bf3b48
Andrew Davisson <akdavisson4@gmail.com>
2019-10-15 13:43:46 -0600

    Added index and show to pictures_controller

--- backend/app/controllers/pictures_controller.rb
+++ backend/app/controllers/pictures_controller.rb
@@ -1,2 +1,16 @@
 class PicturesController < ApplicationController
+
+  def index
+    pictures = Picture.all
+    render json: pictures, only: [:id, :title, :link]
+  end
+
+  def show
+    picture = Picture.find_by(id: params[:id])
+    if picture
+      render json: picture, only: [:id, :title, :link]
+    else
+      render json: {error: "No picture found with matching id."}
+    end
+  end
 end
