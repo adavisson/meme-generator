7733a2a8f940fff80e5a44192a6858e4d78f1298
Andrew Davisson <akdavisson4@gmail.com>
2019-10-21 11:47:01 -0600

    Added create action for phrases controller

--- backend/app/controllers/phrases_controller.rb
+++ backend/app/controllers/phrases_controller.rb
@@ -3,4 +3,9 @@ class PhrasesController < ApplicationController
     phrases = Phrase.all
     render json: phrases, only: [:id, :content]
   end
+
+  def create
+    phrase = Phrase.find_or_create_by(content: params[:content])
+    render json: phrase, only: [:id, :content]
+  end
 end
